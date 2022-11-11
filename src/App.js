121081522df89d18dfead319e14c6038cc9ed4d0
JA9901 <jareyes999@gmail.com>
2022-11-11 15:03:39 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -8,12 +9,11 @@ function App() {
       <h1>Hey clever programmer fam, lets build the google clone</h1>
     
       {/*Home (the one with the search on) */}
+      <Home />
 
       {/* SearchPage (the results page) */}
     </div>
   );
 }
-
-new
 
 export default App;
